0d09dae2218fc2d5ffc4854711ba9d96dc6dbb0d
rikan <quanshang123@qq.com>
2016-02-03 09:36:39 +0800

    alert

--- infoBox.js
+++ infoBox.js
@@ -5,48 +5,87 @@ var infoBox = infoBox || {};
         $el: null,
         options: {},
         _init: function () {
-            var me = this;
             this.$el = this._createBox();
+            if (this.options.movable) {
+                this._makeMovable();
+            }
+
         },
         _createBox: function () {
+            var me = this;
             var $box = $('<div class="infoBox"></div>');
-            if (this.options.hasHeader) {
+
+            if (me.options.hasHeader) {
                 var $header = $('<div class="infoBox-header"></div>');
-                if (this.options.title) {
-                    $header.append('<div class="infoBox-title">' + this.options.title + '</div>');
+
+                if (me.options.movable) {
+                    $header.addClass('movable');
                 }
-                if (this.options.hasClose) {
-                    $header.append('<div class="infoBox-close"></div>');
+                if (me.options.title) {
+                    $header.append('<div class="infoBox-title">' + me.options.title + '</div>');
+                }
+
+                if (me.options.hasClose) {
+                    var $close = $('<div class="infoBox-close"></div>');
+                    $header.append($close);
+                    $close.click(function () {
+                        me._destroy();
+                    })
                 }
                 $box.append($header);
             }
 
-            $box.append('<div class="infoBox-body"></div>');
+            var $body = $('<div class="infoBox-body"></div>');
 
-            if (this.options.hasFooter) {
+            if (me.options.msg) {
+                $body.append('<div class="infoBox-msg">' + me.options.msg + '</div>');
+            }
+            $box.append($body);
+            if (me.options.hasFooter) {
                 var $footer = $('<div class="infoBox-footer"></div>');
-                if (this.options.footerBtns) {
-                    for (var i = 0, j = this.options.footerBtns.length; i < j; i++) {
-                        $footer.append(this._createBtn(this.options.btns[i]));
+                if (me.options.buttons) {
+                    for (var i = 0, j = me.options.buttons.length; i < j; i++) {
+                        $footer.append(me._createBtn(me.options.buttons[i]));
                     }
                 }
                 $box.append($footer);
             }
 
-            if (this.options.modal) {
+            if (me.options.modal) {
                 $box.addClass('modal');
             }
+            $box.appendTo(document.body);
             return $box;
         },
-
+        _processInput: function (options) {
+            return options;
+        },
         _show: function () {
             this.$el.show();
         },
-        _close: function () {
+        _hide: function () {
             this.$el.hide();
         },
         _destroy: function () {
             this.$el.remove();
+        },
+
+        _makeMovable: function () {
+            var me = this;
+            me.$el.mousedown(function () {
+                me.$el.mousemove(function (e) {
+
+                    var thisX = event.pageX - $(this).width() / 2,
+                        thisY = event.pageY - $(this).height() / 2;
+
+                    me.$el.offset({
+                        left: thisX,
+                        top: thisY
+                    });
+                })
+            }).mouseup(function () {
+                me.$el.off('mousemove');
+            })
         },
 
         _createBtn: function () {
@@ -54,9 +93,60 @@ var infoBox = infoBox || {};
         }
     };
 
+    function infoBoxAlert(type, options) {
+        this.type = type;
+
+        this.options = this._processInput(options);
+
+        this._init();
+        this._show();
+    }
+
+    infoBoxAlert.prototype = $.extend({}, infoBoxBase, {
+        constructor: infoBoxAlert,
+        _processInput: function (options) {
+            var mergedOptions = infoBoxBase._processInput.call(this, options);
+
+            options = $.extend({}, mergedOptions, infoBox.alert.base, infoBox.alert.options[this.type]);
+
+            return options;
+        },
+        _init: function () {
+            infoBoxBase._init.call(this);
+        }
+    });
 
     infoBox.alert = function (type, options) {
+        return new infoBoxAlert(type, options);
+    };
 
+    infoBox.alert.base = {
+        buttons: {
+            ok: {
+                text: 'ok',
+                class: ''
+            }
+        },
+        modal: true,
+        hasHeader: true,
+        hasClose: true,
+        hasFooter: true,
+        movable: true
+    };
+
+    infoBox.alert.options = {
+        success: {
+            icon: ''
+        },
+        warning: {
+            icon: ''
+        },
+        danger: {
+            icon: ''
+        },
+        info: {
+            icon: ''
+        }
     }
 
 
